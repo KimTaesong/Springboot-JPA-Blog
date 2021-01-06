d4cffc4809d5a49c385e049779271f1a49454c82
Taesong Kim <taesongweb@gmail.com>
2021-01-07 08:52:23 +0900

    UPDATE README.md

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -15,9 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpControllerTest {
 	
+	private static final String TAG = "HttpControllerTest:";
+	
+	//localhsot:8000/blog/http/lombok
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		//Member m = new Member(1, "ssar", "1234", "email");
+		// builder 패턴 Member.builder() - 내가 원하는 항목을 가져다가 쓸 수 있음, 생성자의 순서는 상관 없음, 그리고 파라미터의 인수 실수를 방지해줌.
+		Member m = Member.builder().username("ssar").password("1234").email("ssar@nate.com").build();
+		System.out.println(TAG + "getter : " + m.getId());
+		m.setId(5000);
+		System.out.println(TAG + "setter : " + m.getId());
+		System.out.println(TAG + "getter : " + m.getUsername());
+		m.setUsername("cos");
+		System.out.println(TAG + "setter : " + m.getUsername());
+		return "lombok test 완료";
+	}
 	//http://localhost:8080/http/get (select)
 	@GetMapping("/http/get")
 	public String getTest(Member m) { //id=1&username=ssar&password=1234&email=ssar@nate.com MessageConverter (스프링부트)
+
 		return "get 요청: " + m.getId() +", " +m.getUsername() +", "+m.getPassword()+", "+m.getEmail();
 	}
 	
--- src/main/java/com/cos/blog/test/Member.java
+++ src/main/java/com/cos/blog/test/Member.java
@@ -1,40 +1,27 @@
 package com.cos.blog.test;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Member {
-	private int id;
-	private String username;
-	private String password;
-	private String email;
+	private  int id;	// final - 불변성 유지 
+	private  String username;
+	private  String password;
+	private  String email;
 	
+	@Builder // lombok을 쓰면 Builder 패턴을 만들어줌
 	public Member(int id, String username, String password, String email) {
 		this.id = id;
 		this.username = username;
 		this.password = password;
 		this.email = email;
 	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
+	
 	
 }
